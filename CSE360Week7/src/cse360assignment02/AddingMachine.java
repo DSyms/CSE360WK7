febb42ef1a9f73ce5c4da3d8ec7db2985ef01e45
David Syms <dsyms1@asu.edu>
2020-10-01 22:54:59 -0700

    Fixed missing comments

--- CSE360Week7/src/cse360assignment02/AddingMachine.java
+++ CSE360Week7/src/cse360assignment02/AddingMachine.java
@@ -1,22 +1,48 @@
 package cse360assignment02;
 
+/**
+ * This is David Syms's Week 7 submission for Dr. Gonzalez Sanchez's CSE 360 Class
+ * Fall 2020 Tuesday 9 am
+ * @author David Syms
+ */
+
 public class AddingMachine {
   private int total;
   
+  /**
+   * This is constructor for the adding machine. 
+   * 
+   */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
+  /**
+   * This returns total value thus far
+   * @return total
+   */
   public int getTotal () {
     return 0;
   }
   
+  /**
+   * Function that adds parameter to total
+   * @param value Value to be added to total
+   */
   public void add (int value) {
   }
 
+  /**
+   * Function that subtracts parameter to total
+   * @param value Value to be added to total
+   */
   public void subtract (int value) {
   }
 
+  /**
+   * Function that returns string of operations preformed thus far
+   * @return operations string
+   */
   public String toString () {
     return "";
   }
